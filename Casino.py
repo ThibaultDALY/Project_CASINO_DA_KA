adfae2697df2c576912ed2b8e5f6909f089593d9
DALY <thibaultdaly@live.fr>
2017-01-26 15:31:34 +0100

    1 step done dict in output perfect

--- Casino.py
+++ Casino.py
@@ -144,7 +144,7 @@ class CASINO(object):
             player_gain1.append(amounts_game1[game][0][1])
             casino_gain1.append(amounts_game1[game][0][0])
 
-        casino_gain1 = sum(casino_gain1)
+        casino_gain1 = casino_gain1
         print(player_gain1)
         print(casino_gain1)
 
@@ -155,7 +155,7 @@ class CASINO(object):
 
         income_game1 = [] # Here are the income of each player after game 1
         for group in range(len(customers_dict2)):
-            for player in range(len(customers_dict2)[group]):
+            for player in range(len(customers_dict2[group])):
                 income_game1.append(customers_dict3[group][player][1] - amounts[group][player] + player_gain1[group][player])
 
         # Create a function to split list
@@ -168,31 +168,52 @@ class CASINO(object):
             arrs.append(arr)
             return arrs
 
-        # income_game1 = split(income_game1, 5)
+        income_game1 = split(income_game1, 5)
+        print(income_game1)
+
+        # Create a function to replace an element in a list
+        def replace_at_index1(tup, ix, val):
+            lst = list(tup)
+            lst[ix] = val
+            return tuple(lst)
+
+        customers_dict4 = []
+        for group in range(len(customers_dict2)):
+            loop_dico = []
+            for player in range(len(customers_dict2[group])):
+                loop_dico.append(replace_at_index1(customers_dict3[group][player],1,income_game1[group][player]))
+            customers_dict4.append(loop_dico)
+
+        print(amounts)
+        print(customers_dict2)
+        print(customers_dict4) # Here is the income of each player after the game 1 !!
+
+        """ Let's now keep only players with income > 0 after the game 1 """
+
+        # Here is the final list of income for each player
+        customers_dict5 = []
+        for group in customers_dict4:
+            customers_dict5.append([i for i in group if i[1] > 0])
+        print(customers_dict5)
         #
-        # # Create a function to replace an element in a list
-        # def replace_at_index1(tup, ix, val):
-        #     lst = list(tup)
-        #     lst[ix] = val
-        #     return tuple(lst)
-        #
-        # customers_dict4 = []
-        # for group in range(len(customers_dict2)):
-        #     loop_dico = []
-        #     for player in range(len(customers_dict2[group])):
-        #         loop_dico.append(replace_at_index1(customers_dict3[group][player],1,income_game1[group][player]))
-        #     customers_dict4.append(loop_dico)
-        #
-        # print(amounts)
-        # print(customers_dict2)
-        # print(customers_dict4) # Here is the income of each player after the game 1 !!
-        #
-        # for group in range(len(customers_dict2)):
-        #     for player in range(len(customers_dict2[group])):
-        #         if customers_dict4[group][player][1] == 0:
-        #             customers_dict4[group][player].pop()
-        #         else:
-        #             break
-        # print(customers_dict4)
+        Bal = []
+        for group in customers_dict5:
+            Bal.append(dict(group))
+        print(Bal)
+
+        # Re-transform the output into dico form
+        player_game1_int = []
+        for group in customers_dict5:
+            player_game1_int.append(dict(group))
+        print(player_game1_int)
+
+        # Transform the different dico in a unique dictionary
+        from functools import reduce
+        def update(d, other):
+            d.update(other)
+            return d
+
+        player_game1 = reduce(update, player_game1_int, {})
+        print(player_game1)
 
 CASINO(500, 2, 2, 3, 200, 22, 10, 2).SimulateEvening(1)
